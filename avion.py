0beb08b9c0affe37a71fe05e89769815759e6027
Anas SHAIK <anas.shaik@ens.uvsq.fr>
2021-04-19 10:03:51 +0200

    creation quadrillage

--- avion.py
+++ avion.py
@@ -9,11 +9,61 @@
 
 # https://github.com/uvsq21918039/Projet-Avion
 ######
-import tkinter as tk
+######
+#########################################################
+# import des librairies
+import tkinter as tk 
+########################################################
+
+
+#########################################################
+#définition des constantes
+
+LARGEUR=45
+HAUTEUR=30
+COULEUR_FOND ="white"
+COULEUR_PLACE_LIBRE="green"
+COULEUR_PLACE_OCCUPE="red"
+
+#########################################################
+
+#########################################################
+#définition des variables globales
+
+
+
+
+
+
+
+
+#########################################################
+
+
+#########################################################
+#définition des fonctions 
+
+def quadrillage():
+    """Affiche un quadrillage constitué de carrés de côté COTE_PARCELLE"""
+    y = 2
+    while y < 8*HAUTEUR:
+        canvas.create_line((2, y), (30*LARGEUR, y), fill="black")
+        y += HAUTEUR
+    x = 2 
+    while x < 31*LARGEUR:
+        canvas.create_line((x, 0), (x, 7*HAUTEUR), fill="black")
+        x += LARGEUR
+ #########################################################
+
+#########################################################
+#programme principal
 
 racine = tk.Tk()
-canvas = tk.Canvas(racine, bg='green', height=1400, width=630)
+canvas = tk.Canvas(racine,width=30*LARGEUR,height=7*HAUTEUR,bg=COULEUR_FOND)
 canvas.grid()
 
-racine.mainloop
+quadrillage()
+racine.mainloop()
+########################################################
+
 
